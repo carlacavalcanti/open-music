4f4005d3a393325c87b40c271ed183a2694ecdbf
Carla Cavalcanti <carlinhacavalcanti17@gmail.com>
2026-01-26 20:59:35 -0300

    Adiciona funções

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 import { initUi } from "./ui.js";
+import { applyInputRangeStyle } from "./inputRange.js";
+import { albumList } from "./albumsDatabase.js";
 
 initUi();
+applyInputRangeStyle();
+
+const cards = document.querySelector(".cards");
+
+function creatCard(album) {
+  const divCard = document.createElement("div");
+  divCard.classList.add("card");
+
+  const img = document.createElement("img");
+  img.src = album.img;
+
+  const title = document.createElement("h2");
+  title.innerText = album.title;
+
+  const divFlex = document.createElement("div");
+  divFlex.classList.add("flex", "name");
+  const nameArtist = document.createElement("p");
+  nameArtist.innerText = album.band;
+  const nameGenero = document.createElement("p");
+  nameGenero.innerText = album.genre;
+
+  divFlex.append(nameArtist, nameGenero);
+
+  const divValor = document.createElement("div");
+  divValor.classList.add("flex");
+  const valor = document.createElement("p");
+  valor.innerText = album.price;
+  valor.classList.add("valor");
+  const button = document.createElement("button");
+  button.innerText = "Comprar";
+
+  divValor.append(valor, button);
+  divCard.append(img, title, divFlex, divValor);
+  cards.appendChild(divCard);
+}
+
+function renderAlbuns(lista) {
+  cards.innerHTML = "";
+  lista.forEach((album) => {
+    creatCard(album);
+  });
+}
+renderAlbuns(albumList);
--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -12,20 +12,14 @@ export function initUi() {
 
   const botaoModo = document.querySelector(".modo");
   const body = document.querySelector("body");
-  const cards = document.querySelectorAll(".card");
-  const cabecalho = document.querySelector(".header-fixed");
 
   botaoModo.addEventListener("click", () => {
     body.classList.toggle("modo-escuro");
-    cabecalho.classList.toggle("modo-escuro");
-    if (cabecalho.classList.contains("modo-escuro")) {
+
+    if (body.classList.contains("modo-escuro")) {
       botaoModo.src = "./src/assets/icons/sun.svg";
     } else {
       botaoModo.src = "./src/assets/icons/moon.svg";
     }
-
-    cards.forEach((card) => {
-      card.classList.toggle("card-modo-escuro");
-    });
   });
 }
